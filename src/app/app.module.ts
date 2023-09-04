ffda5110c38071ed8691f95f780daf7520644714
SathishKumar013 <devilsk567@gmail.com>
2023-09-04 11:04:54 +0530

    Initial commit for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { DataComponent } from './data/data.component';
+import { AccessComponent } from './access/access.component';
+import { DigitalComponent } from './digital/digital.component';
+import { SoftwareComponent } from './software/software.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent,
+    AccessComponent,
+    DigitalComponent,
+    SoftwareComponent,
+
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
